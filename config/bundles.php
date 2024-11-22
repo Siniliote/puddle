d92e2c22f4389ee056a9c24aeb2b019511567853
Stéphane FEREY <stephane.ferey@gmail.com>
2024-11-22 21:44:01 +0100

    chore: Add symfony/asset-mapper symfony/asset symfony/twig-pack

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
